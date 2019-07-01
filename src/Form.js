c1757c49d6f17873ff1dd99d526730149ac33318
Yaroslav <doctor355@yahoo.com>
2019-07-01 10:33:59 +0300

    get value and bind at name
    Change of state value in one function for two values.

--- src/Form.js
+++ src/Form.js
@@ -1,38 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Form = () => {
-const [values, setValues] = useState({email:'', password:''});
-const handleChange = event => {
-  console.log(event.target.name);
-  console.log(event.target.value);
-}
-  return <div>
-    <form noValidate>
-      <div>
-      <label>Email</label>
+  const [values, setValues] = useState({ email: "", password: "" });
+  const handleChange = event => {
+    const { name, value } = event.target;
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+    setValues({
+      ...values,
+      [name]: value
+      // [event.target.name] : event.target.value
+    });
+  };
+  return (
+    <div>
+      <form noValidate>
         <div>
-         <input name="email"
-          type="email"
-           value={values.email} 
-           onChange={handleChange}
-           />
-         {/* for output errors */}
-        </div> 
-      </div>
-      <div>
-      <label>Password</label>
+          <label>Email</label>
+          <div>
+            <input
+              name="email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {/* for output errors */}
+          </div>
+        </div>
         <div>
-         <input name="password"
-          type="password"
-           value={values.password} 
-           onChange={handleChange}
-           />
-         {/* for output errors */}
-        </div> 
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-    
-  </div>
-}
+          <label>Password</label>
+          <div>
+            <input
+              name="password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            {/* for output errors */}
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 export default Form;
